8b17794a86c2f8d92dfd1bbe321d376745b2f183
rich-gupta <rich.gupta@games24x7.com>
2019-12-26 10:56:24 +0530

    implicit wait increased to 10 sec

--- src/main/java/UI/Pages/AbstractBasePageWeb.java
+++ src/main/java/UI/Pages/AbstractBasePageWeb.java
@@ -81,7 +81,7 @@ public class AbstractBasePageWeb {
      */
     public WebElement findByWait(By locatn) {
         WebElement element = null;
-        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         try {
             WebDriverWait wait = new WebDriverWait(driver, 60);
             wait.until(ExpectedConditions.presenceOfElementLocated(locatn));
